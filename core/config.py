e881ada3897e164f9e3c09707e10a722defa6182
bpsc-dev-backend <bpsc.2026@outlook.com>
2026-06-19 03:28:36 +0530

    first commit

--- core/config.py
+++ core/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Indic-RAG Knowledge Base"
     DATABASE_URL: str = os.getenv("DATABASE_URL")
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
     
     class Config:
         env_file = ".env"
